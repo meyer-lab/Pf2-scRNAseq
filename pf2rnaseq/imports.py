7a9a2007889fe87c0a19501a01548fcbf28ff74e
Nicole Bedanova <nbedanova@g.ucla.edu>
2025-07-01 10:31:24 -0700

    Using prepare dataset from parafac2 repo

--- pf2rnaseq/imports.py
+++ pf2rnaseq/imports.py
@@ -1,5 +1,5 @@
 from concurrent.futures import ProcessPoolExecutor
-
+from parafac2.normalize import prepare_dataset
 import anndata
 import numpy as np
 import pandas as pd
@@ -7,78 +7,6 @@ import scanpy as sc
 from scipy.sparse import csr_array, spmatrix
 from sklearn.preprocessing import scale
 from sklearn.utils.sparsefuncs import inplace_column_scale, mean_variance_axis
-
-
-def prepare_dataset_deviance(
-    X: anndata.AnnData, condition_name, geneThreshold
-) -> anndata.AnnData:
-    X.X = csr_array(X.X)  # type: ignore
-    assert np.amin(X.X.data) >= 0.0
-    # Remove cells and genes with fewer than 10 reads
-    X = X[X.X.sum(axis=1) > 10, X.X.sum(axis=0) > 10]
-    readmean, _ = mean_variance_axis(X.X, axis=0)  # type: ignore
-    X = X[:, readmean > geneThreshold]
-    # Copy so that the subsetting is preserved
-    X._init_as_actual(X.copy())
-    # deviance transform
-    y_ij = X.X.toarray()  # type: ignore
-    # counts per cell
-    n_i = y_ij.sum(axis=1)
-    # MLE of gene expression
-    pi_j = y_ij.sum(axis=0) / np.sum(n_i)
-
-    non_y_ij = n_i[:, None] - y_ij
-    mu_ij = n_i[:, None] * pi_j[None, :]
-    signs = np.sign(y_ij - mu_ij)
-
-    first_term = 2 * y_ij * np.log(np.maximum(y_ij, 1.0) / mu_ij)
-    second_term = 2 * non_y_ij * np.log(non_y_ij / (n_i[:, None] - mu_ij))
-
-    X.X = signs * np.sqrt(np.maximum(first_term + second_term, 0.0))
-
-    X.X = scale(X.X)
-
-    _, sgIndex = np.unique(X.obs_vector(condition_name), return_inverse=True)
-    X.obs["condition_unique_idxs"] = sgIndex
-    X.obs["condition_unique_idxs"] = X.obs["condition_unique_idxs"].astype("category")
-    # Pre-calculate gene means
-    X.var["means"] = np.zeros(X.shape[1])
-
-    assert np.all(np.isfinite(X.X))  # type: ignore
-    return X
-
-
-def prepare_dataset(
-    X: anndata.AnnData, condition_name: str, geneThreshold: float
-) -> anndata.AnnData:
-    assert isinstance(X.X, spmatrix)
-    assert np.amin(X.X.data) >= 0.0  # type: ignore
-
-    # Filter out genes with too few reads
-    readmean, _ = mean_variance_axis(X.X, axis=0)  # type: ignore
-    X = X[:, readmean > geneThreshold]
-    X._init_as_actual(X.copy())
-    # Normalize read depth
-    sc.pp.normalize_total(X, exclude_highly_expressed=False, inplace=True)
-
-    # Scale genes by sum
-    readmean, _ = mean_variance_axis(X.X, axis=0)  # type: ignore
-    readsum = X.shape[0] * readmean
-    inplace_column_scale(X.X, 1.0 / readsum)
-
-    # Transform values
-    X.X.data = np.log10((1000.0 * X.X.data) + 1.0)  # type: ignore
-
-    # Get the indices for subsetting the data
-    _, sgIndex = np.unique(X.obs_vector(condition_name), return_inverse=True)
-    X.obs["condition_unique_idxs"] = sgIndex
-    X.obs["condition_unique_idxs"] = X.obs["condition_unique_idxs"].astype("category")
-
-    # Pre-calculate gene means
-    means, _ = mean_variance_axis(X.X, axis=0)  # type: ignore
-    X.var["means"] = means
-
-    return X
 
 
 def import_citeseq() -> anndata.AnnData:
@@ -134,7 +62,7 @@ def import_Heiser(deviance=False) -> anndata.AnnData:
     data = anndata.read_h5ad("/home/nicoleb/C3TAg.h5ad")
     if deviance:
         # Apply deviance transformation
-        return prepare_dataset_deviance(data, "sample_id", geneThreshold=0.1)
+        return prepare_dataset(data, "sample_id", geneThreshold=0.1, deviance=True)
     else:
         # Apply standard normalization and scaling
         return prepare_dataset(data, "sample_id", geneThreshold=0.1)
