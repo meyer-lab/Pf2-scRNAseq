19d0d3960ec8cf35456741dd40d69bfc5961c2c2
Nicole Bedanova <nbedanova@g.ucla.edu>
2025-07-21 16:57:08 -0700

    Deviance error

---
+++ pf2rnaseq/figures/figureHeiserFactors.py
@@ -0,0 +1,54 @@
+"""
+Heiser: Plotting factors
+"""
+
+import pandas as pd
+import numpy as np
+from ..factorization import correct_conditions, pf2
+from ..imports import import_Heiser
+from .common import getSetup, subplotLabel
+from .commonFuncs.plotFactors import (
+    plot_condition_factors_groups,
+    plot_eigenstate_factors,
+    plot_factor_weight,
+    plot_gene_factors,
+)
+
+
+def samples_only(X) -> pd.DataFrame:
+    """Obtain samples once only with corresponding observations"""
+    samples = X.obs
+    df_samples = samples.drop_duplicates(subset="condition_unique_idxs")
+    df_samples = df_samples.sort_values("condition_unique_idxs")
+    return df_samples
+
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure."""
+    # Get list of axis objects
+    ax, f = getSetup((15, 15), (2, 2))
+
+    # Add subplot labels
+    subplotLabel(ax)
+    X = import_Heiser(deviance=True)
+    print(f"Data shape: {X.X.shape}")
+    print(f"Data type: {X.X.dtype}")
+    print(f"Contains NaN: {np.isnan(X.X).any()}")
+    print(f"Contains inf: {np.isinf(X.X).any()}")
+    print(f"Min value: {X.X.min()}")
+    print(f"Max value: {X.Xmax()}")
+
+    X = pf2(X, 20)
+
+    stimulations = samples_only(X)["treatment"]
+    tumors = samples_only(X)["expBatch"]
+    print(stimulations)
+
+    plot_condition_factors_groups(
+        X, ax[0], stimulations, tumors, cond="sample_id", groupConditions=True
+    )
+    plot_eigenstate_factors(X, ax[1])
+    plot_gene_factors(X, ax[2])
+    plot_factor_weight(X, ax[3])
+
+    return f
--- pf2rnaseq/imports.py
+++ pf2rnaseq/imports.py
@@ -1,123 +1,11 @@
-import glob
-from concurrent.futures import ProcessPoolExecutor
-from pathlib import Path
 
+from parafac2.normalize import prepare_dataset
 import anndata
-import numpy as np
-import pandas as pd
-import scanpy as sc
-from scipy.sparse import csr_matrix, spmatrix
-from sklearn.utils.sparsefuncs import inplace_column_scale, mean_variance_axis
 
 
-def prepare_dataset(
-    X: anndata.AnnData, condition_name: str, geneThreshold: float
-) -> anndata.AnnData:
-    assert isinstance(X.X, spmatrix)
-    assert np.amin(X.X.data) >= 0.0  # type: ignore
-
-    # Filter out genes with too few reads
-    readmean, _ = mean_variance_axis(X.X, axis=0)  # type: ignore
-    X = X[:, readmean > geneThreshold]
-    X._init_as_actual(X.copy())
-    # Normalize read depth
-    sc.pp.normalize_total(X, exclude_highly_expressed=False, inplace=True)
-
-    # Scale genes by sum
-    readmean, _ = mean_variance_axis(X.X, axis=0)  # type: ignore
-    readsum = X.shape[0] * readmean
-    inplace_column_scale(X.X, 1.0 / readsum)
-
-    # Transform values
-    X.X.data = np.log10((1000.0 * X.X.data) + 1.0)  # type: ignore
-
-    # Get the indices for subsetting the data
-    _, sgIndex = np.unique(X.obs_vector(condition_name), return_inverse=True)
-    X.obs["condition_unique_idxs"] = sgIndex
-    X.obs["condition_unique_idxs"] = X.obs["condition_unique_idxs"].astype("category")
-
-    # Pre-calculate gene means
-    means, _ = mean_variance_axis(X.X, axis=0)  # type: ignore
-    X.var["means"] = means
-
-    return X
 
 
-def import_citeseq() -> anndata.AnnData:
-    """Imports 5 datasets from Hamad CITEseq."""
-    files = ["control", "ic_pod1", "ic_pod7", "sc_pod1", "sc_pod7"]
-
-    with ProcessPoolExecutor(max_workers=5) as executor:
-        futures = [
-            executor.submit(
-                sc.read_10x_mtx,
-                "/opt/andrew/HamadCITEseq/" + k,
-                gex_only=False,
-                make_unique=True,
-            )
-            for k in files
-        ]
-
-        data = {k: futures[i].result() for i, k in enumerate(files)}
-
-    X = anndata.concat(data, merge="same", label="Condition")
-
-    return prepare_dataset(X, "Condition", geneThreshold=0.1)
-
-
-def import_HTAN() -> anndata.AnnData:
-    """Imports Vanderbilt's HTAN 10X data."""
-    files = glob.glob("/opt/extra-storage/HTAN/*.mtx.gz")
-    futures = []
-    data = {}
-
-    with ProcessPoolExecutor(max_workers=10) as executor:
-        for filename in files:
-            future = executor.submit(
-                sc.read_10x_mtx,
-                "/opt/extra-storage/HTAN/",
-                gex_only=False,
-                make_unique=True,
-                prefix=filename.split("/")[-1].split("matrix.")[0],
-            )
-            futures.append(future)
-
-        for i, k in enumerate(files):
-            result = futures[i].result()
-            data[k.split("/")[-1].split("_matrix.")[0]] = result
-
-    X = anndata.concat(data, merge="same", label="Condition")
-
-    return prepare_dataset(X, "Condition", geneThreshold=0.1)
-
-
-def import_CCLE() -> anndata.AnnData:
-    """Imports barcoded cell data."""
-    # TODO: Still need to add gene names and barcodes.
-    folder = "/opt/extra-storage/asm/Heiser-barcode/CCLE/"
-
-    adatas = {
-        "HCT116_1": anndata.read_text(
-            Path(folder + "HCT116_tracing_T1.count_mtx.tsv")
-        ).T,
-        "HCT116_2": anndata.read_text(
-            Path(folder + "HCT116_tracing_T2.count_mtx.tsv")
-        ).T,
-        "MDA-MB-231_1": anndata.read_text(
-            Path(folder + "MDA-MB-231_tracing_T1.count_mtx.tsv")
-        ).T,
-        "MDA-MB-231_2": anndata.read_text(
-            Path(folder + "MDA-MB-231_tracing_T2.count_mtx.tsv")
-        ).T,
-    }
-
-    X = anndata.concat(adatas, label="sample")
-    X.X = csr_matrix(X.X)
-
-    return prepare_dataset(X, "sample", geneThreshold=0.1)
-
-
-def import_cytokine() -> anndata.AnnData:
+def import_cytokine(deviance=False) -> anndata.AnnData:
     """Import Meyer Cytokine PBMC dataset.
     -- columns from observation data:
     {'Stimulation': Cytokine and Dose}
@@ -127,7 +15,9 @@ def import_cytokine() -> anndata.AnnData:
     # Remove multiplexing identifiers
     X = X[:, ~X.var_names.str.match("^CMO3[0-9]{2}$")]  # type: ignore
 
-    return prepare_dataset(X, "Condition", geneThreshold=0.002)  # 0.1
+    return prepare_dataset(
+        X, "Condition", geneThreshold=0.002, deviance=deviance
+    )  # 0.1
 
 
 def import_pf2Cytokine30() -> anndata.AnnData:
@@ -140,60 +30,33 @@ def import_pf2Cytokine30() -> anndata.AnnData:
     return X
 
 
-def import_Heiser() -> anndata.AnnData:
+def import_Heiser(deviance=False) -> anndata.AnnData:
     """Import Heiser C3TAg dataset.
     anndata.X is the raw counts
 
     """
     data = anndata.read_h5ad("/home/nicoleb/C3TAg.h5ad")
 
-    return prepare_dataset(data, "sample_id", geneThreshold=0.01)
+    return prepare_dataset(
+        data, "sample_id", geneThreshold=0.1, deviance=deviance
+    )  
 
 
-def import_MouseImmune() -> anndata.AnnData:
-    """Import cytokine data including gene expression and hashtag information.
-    Processes files with naming patterns like:
-    - GSM6102842_cytokine-samples07-barcodes.tsv.gz
-    - GSM6102885_cytokine-hashtags06-matrix.mtx.gz
-    """
+def import_MouseImmune(geneThreshold=0.1, deviance=False) -> anndata.AnnData:
+    """Import Mouse Immune Dictionary cytokine data.
+     -- columns from observation data:
+    {'biosample_id': cytokine and replicate info,
+    'rep': replicate,
+    'species': mouse species,
+    'cytokine_family': cytokine family label,
+    'cyt': cytokine mouse was treated with,
+    'sex': sex of mouse,
+    'celltype': cell type label,
+    'organ__ontology_label': organ label,
+    ...}"""
     X = anndata.read_h5ad("/home/nicoleb/MouseCytok.h5ad")
+    # Filter out doublets
+    X = X[X.obs["celltype"] != "doublet", :]
+    X = X.copy()
 
-    return prepare_dataset(X, "cyt", geneThreshold=0.1)  # 0.01
-
-
-def pseudobulk_lupus(X, cellType="Cell Type"):
-    """Average gene expression for each condition and cell type;
-    creates matrix and tensor version"""
-    X_df = X.to_df()
-    X_df = X_df.subtract(X.var["means"].values)
-    X_df["Condition"] = X.obs["Condition"].values
-    X_df["Cell Type"] = X.obs[cellType].values
-    X_df["Status"] = X.obs["SLE_status"].values
-    X_matrix = (
-        X_df.groupby(["Condition", "Cell Type"], observed=False)
-        .mean(numeric_only=True)
-        .reset_index()
-    )
-
-    conds = pd.unique(X_matrix["Condition"])
-    celltypes = pd.unique(X_matrix["Cell Type"])
-    genes = X.var_names.values
-
-    status = []
-    for i, cond in enumerate(conds):
-        all_status = X_df.loc[X_df["Condition"] == cond]["Status"]
-        status = np.append(status, np.unique(all_status))
-
-    X_matrix["Status"] = np.repeat(status, len(celltypes))
-
-    X_tensor = np.empty((len(conds), len(celltypes), len(genes)))
-    X_tensor[:] = np.nan
-
-    for i, cond in enumerate(conds):
-        for j, celltype in enumerate(celltypes):
-            specific_df = X_matrix.loc[
-                (X_matrix["Condition"] == cond) & (X_matrix["Cell Type"] == celltype)
-            ]
-            X_tensor[i, j, :] = specific_df.iloc[0, 2:-1].to_numpy()
-
-    return X_matrix, X_tensor
+    return prepare_dataset(X, "biosample_id", geneThreshold, deviance=deviance)  # 0.01
