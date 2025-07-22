8c66564329f2855160cbd55a89faca16fcc67baf
Aaron Meyer <ameyer@ucla.edu>
2025-07-21 20:39:03 -0700

    Add testing

--- pf2rnaseq/tests/test_import.py
+++ pf2rnaseq/tests/test_import.py
@@ -4,16 +4,34 @@ Test the cross validation accuracy.
 
 import numpy as np
 import pytest
+from scipy.sparse import csr_array
 
-from ..imports import import_CCLE, import_citeseq, import_HTAN
+from ..imports import import_cytokine, import_Heiser, import_MouseImmune
 
 
 @pytest.mark.parametrize(
     "import_func",
-    [import_citeseq, import_HTAN, import_CCLE],
+    [import_cytokine, import_MouseImmune],
 )
 def test_imports(import_func):
     """Test import functions."""
     X = import_func()
     print(f"Data shape: {X.shape}")
     assert X.X.dtype == np.float32
+
+
+@pytest.mark.parametrize(
+    "deviance",
+    [False, True],
+)
+def test_import_Heiser(deviance):
+    """Test import functions."""
+    X = import_Heiser(deviance)
+    print(f"Data shape: {X.shape}")
+
+    Xdata: csr_array | np.ndarray = X.X # type: ignore
+
+    if isinstance(X.X, csr_array):
+        assert np.all(np.isfinite(Xdata.data))
+    else:
+        assert np.all(np.isfinite(Xdata)) # type: ignore
